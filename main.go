a2c50c9644713aff921de29fd79e36dfcfbdbc38
Roberto Ortega <berto.ort@gmail.com>
2018-01-24 16:09:04 -0700

    :bug: fix port

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi"
 )
@@ -14,11 +15,12 @@ type turtle struct {
 }
 
 func main() {
+	port := os.Getenv("PORT")
 	router := chi.NewRouter()
 	donatelo := turtle{Name: "Donatelo", Age: 100, Color: "purple"}
 	router.Get("/", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(writer).Encode(donatelo)
 	})
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":"+port, router)
 }
