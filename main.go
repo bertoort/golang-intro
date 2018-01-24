0319e46b11fd1fe72d78d5db7a7705b470d71d4f
Roberto Ortega <berto.ort@gmail.com>
2018-01-24 16:06:55 -0700

    🚀 ready for deployment

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
+type turtle struct {
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Color string `json:"color"`
+}
 
 func main() {
-	fmt.Println("Hello Go!")
+	router := chi.NewRouter()
+	donatelo := turtle{Name: "Donatelo", Age: 100, Color: "purple"}
+	router.Get("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(writer).Encode(donatelo)
+	})
+	http.ListenAndServe(":3000", router)
 }
